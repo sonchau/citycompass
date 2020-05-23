e2422d195b41c24a55d434443519875ac873c39f
Usman Haider <uhaider.bese15seecs@seecs.edu.pk>
2020-05-23 18:30:27 +0500

    WIP: populationhighlights page styling

--- src/views/PopulationHightLights.jsx
+++ src/views/PopulationHightLights.jsx
@@ -1,5 +1,10 @@
 import React from "react";
-import palace from "../img/bunjil_palace.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faMapMarkerAlt,
+  faVectorSquare,
+  faBuilding,
+} from "@fortawesome/free-solid-svg-icons";
 
 const PopulationHighlights = () => {
   return (
@@ -19,36 +24,84 @@ const PopulationHighlights = () => {
               flexDirection: "column",
               justifyContent: "space-between",
               flexGrow: 1,
-              backgroundColor: "purple",
               marginLeft: "1rem",
+              color: "rgb(255, 255, 255)",
+              fontSize: "2.5rem",
             }}
           >
             <div
               style={{
-                flex: "0 0 30%",
-                backgroundColor: "pink",
+                flex: "0 0 31%",
+                backgroundColor: "rgb(133, 20, 72)",
+                padding: "2rem",
               }}
             >
-              first card
+              <p>
+                <FontAwesomeIcon icon={faMapMarkerAlt} /> Population
+              </p>
+              <p
+                style={{
+                  textAlign: "center",
+                  fontSize: "6rem",
+                  paddingTop: "0.5rem",
+                }}
+              >
+                353,872
+              </p>
             </div>
             <div
               style={{
-                flex: "0 0 30%",
-                backgroundColor: "green",
+                flex: "0 0 31%",
+                backgroundColor: "rgb(241, 66, 29)",
+                padding: "2rem",
               }}
             >
-              second card
+              <p>
+                <FontAwesomeIcon icon={faVectorSquare} /> Area
+              </p>
+
+              <p
+                style={{
+                  textAlign: "center",
+                  fontSize: "6rem",
+                  paddingTop: "0.5rem",
+                }}
+              >
+                409 km 2
+              </p>
             </div>
             <div
               style={{
-                flex: "0 0 30%",
-                backgroundColor: "orange",
+                flex: "0 0 31%",
+                backgroundColor: "rgb(211, 24, 59)",
+                padding: "2rem",
               }}
             >
-              third card
+              <p>
+                <FontAwesomeIcon icon={faBuilding} /> Density
+              </p>
+
+              <p
+                style={{
+                  textAlign: "center",
+                  fontSize: "4rem",
+                  paddingTop: "0.8rem",
+                }}
+              >
+                862 people/km2
+              </p>
             </div>
           </div>
         </div>
+        <p>
+          The City of Casey is located in Melbourne's south-eastern suburbs,
+          between 28 and 60 kilometres south-east of the Melbourne CBD. The City
+          of Casey is bounded by the City of Knox and the Yarra Ranges Council
+          area in the north, Cardinia Shire in the east.
+        </p>
+        <div
+          style={{ width: "100%", height: "100rem", backgroundColor: "black" }}
+        ></div>
       </div>
     </React.Fragment>
   );
