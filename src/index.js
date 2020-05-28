3c8c9609611dd01871d2f23dc3e7b013127cfb0b
“Son <son.chau@geografia.com.au>
2020-05-28 15:42:07 +1000

    [#8] add route and axios

--- src/index.js
+++ src/index.js
@@ -8,22 +8,38 @@ import { Provider } from "react-redux";
 import store from "./store";
 import "./utils/object_extensions.exec.js"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <Switch>
-          {indexRoutes.map((prop, key) => {
-            return (
-              <Route path={prop.path} key={key} component={prop.component} />
-            );
+import { PAGE_DIRECTORY_QUERY1 } from "./sqlQueries";
+import {getData} from './utils/common';
+import RootComponent from "./containers/Root";
+const [,client, page] = window.location.pathname.split('/')
+const clientName = client === "" ? "casey" : client
+
+getData(PAGE_DIRECTORY_QUERY1(clientName)).then (response => {
+  console.log('response from main', response.data)
+  ReactDOM.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Switch>
+          <Route exact path="/" component={RootComponent} />
+          {response.data.rows.map((prop, key) => {
+            const path = `/${clientName}/${prop.page_code}`
+            console.log('path', path)
+              return <Route path={path} key={key} component={RootComponent} /> 
           })}
-        </Switch>
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+          </Switch>
+        </BrowserRouter>
+      </Provider>
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+}).catch (
+  ReactDOM.render(
+    <p>404 - Page not found</p>,
+    document.getElementById("root")
+  )
+)
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/sqlQueries.js
+++ src/sqlQueries.js
@@ -6,3 +6,12 @@ export const PAGE_DIRECTORY_QUERY = `/* PAGE_DIRECTORY_QUERY */ SELECT
   page_code
   FROM casey.cc_pagedirectory_21610
   ORDER BY a_level, b_level, c_level, d_level`;
+
+  export const PAGE_DIRECTORY_QUERY1 = (client) => `/* PAGE_DIRECTORY_QUERY */ SELECT
+  a_level, a_title,
+  b_level, b_title,
+  c_level, c_title,
+  d_level, d_title,
+  page_code
+  FROM ${client}.cc_pagedirectory_21610
+  ORDER BY a_level, b_level, c_level, d_level`;  
