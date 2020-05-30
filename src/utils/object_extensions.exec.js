0d33182a9fa0bea426cc71ef9d78d9ea58d26741
Shoaib Burq <saburq@gmail.com>
2020-05-30 22:02:29 +0200

    handle subnav pages #8

--- src/utils/object_extensions.exec.js
+++ src/utils/object_extensions.exec.js
@@ -58,3 +58,19 @@ const addChain = (receiver) =>
   addDebug(receiver);
   addChain(receiver);
 });
+
+// Object.byString(pageDirectory, pageCodeToObjectPath("A1B2"))
+Object.byString = function (o, s) {
+  s = s.replace(/\[(\w+)\]/g, ".$1"); // convert indexes to properties
+  s = s.replace(/^\./, ""); // strip a leading dot
+  var a = s.split(".");
+  for (var i = 0, n = a.length; i < n; ++i) {
+    var k = a[i];
+    if (k in o) {
+      o = o[k];
+    } else {
+      return;
+    }
+  }
+  return o;
+};
