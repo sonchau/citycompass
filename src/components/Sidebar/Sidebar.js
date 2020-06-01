737621327bb9b20451805adde055e1ebce2153c9
Usman Haider <uhaider.bese15seecs@seecs.edu.pk>
2020-06-01 17:15:42 +0500

    routing working with menuItems in Sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,11 +1,19 @@
 import React from "react";
 import { Menu } from "antd";
+import { useHistory } from "react-router-dom";
+
 const Sidebar = ({ pageDirectory, clientName, setPageMetaData }) => {
-  console.log("pageDirectory", pageDirectory);
+  let history = useHistory();
+  const handleItemClick = (titles, pageCode) => {
+    history.push(`/${clientName}/${pageCode}`);
+    setPageMetaData(titles);
+  };
   return (
     <Menu mode="vertical">
       {pageDirectory.map((a) => (
-        <Menu.ItemGroup title={a["a_title"]}>{bLevel(a.b)}</Menu.ItemGroup>
+        <Menu.ItemGroup title={a["a_title"]}>
+          {renderMenu(a, handleItemClick)}
+        </Menu.ItemGroup>
       ))}
     </Menu>
   );
@@ -13,30 +21,68 @@ const Sidebar = ({ pageDirectory, clientName, setPageMetaData }) => {
 
 export default Sidebar;
 
-function bLevel(b) {
-  return b.map((b) =>
+function renderMenu(a, handleItemClick) {
+  return a.b.map((b) =>
     b.c.length ? (
       <Menu.SubMenu key={b["page_code"]} title={b["b_title"]}>
-        {flyoutMenus(b.c)}
+        {b.c.map((c) => {
+          return c.d.length ? (
+            <Menu.ItemGroup title={c["c_title"]}>
+              {c.d.map((d) => (
+                <Menu.Item
+                  key={d["page_code"]}
+                  onClick={() =>
+                    handleItemClick(
+                      {
+                        a_title: a["a_title"],
+                        b_title: b["b_title"],
+                        c_title: c["c_title"],
+                        d_title: d["d_title"],
+                      },
+                      d["page_code"]
+                    )
+                  }
+                >
+                  {d["d_title"]}
+                </Menu.Item>
+              ))}
+            </Menu.ItemGroup>
+          ) : (
+            <Menu.Item
+              key={c["page_code"]}
+              onClick={() =>
+                handleItemClick(
+                  {
+                    a_title: a["a_title"],
+                    b_title: b["b_title"],
+                    c_title: c["c_title"],
+                  },
+                  c["page_code"]
+                )
+              }
+            >
+              {c["c_title"]}
+            </Menu.Item>
+          );
+        })}
       </Menu.SubMenu>
     ) : (
-      <Menu.Item key={b["page_code"]}>{b["b_title"]}</Menu.Item>
+      <Menu.Item
+        key={b["page_code"]}
+        onClick={() =>
+          handleItemClick(
+            {
+              a_title: a["a_title"],
+              b_title: b["b_title"],
+            },
+            b["page_code"]
+          )
+        }
+      >
+        {b["b_title"]}
+      </Menu.Item>
     )
   );
-}
-
-function flyoutMenus(c) {
-  return c.map((c) => {
-    return c.d.length ? (
-      <Menu.ItemGroup title={c["c_title"]}>
-        {c.d.map((d) => (
-          <Menu.Item key={d["page_code"]}>{d["d_title"]}</Menu.Item>
-        ))}
-      </Menu.ItemGroup>
-    ) : (
-      <Menu.Item key={c["page_code"]}>{c["c_title"]}</Menu.Item>
-    );
-  });
 }
 
 // return (
