26edcda0e549f045122c40e5450df14a11a23658
Usman Haider <uhaider.bese15seecs@seecs.edu.pk>
2020-06-08 12:43:46 +0500

    active color changed

--- src/constants/theme.js
+++ src/constants/theme.js
@@ -12,8 +12,9 @@ const lightTheme = {
   // sidebar section
   sidebarBg: "rgb(255, 255, 255)",
   sidebarHeadingClr: "rgb(38,38,38)",
+
   sidebarLinkClr: "rgb(38,38,38)",
-  sidebarActiveClr: "rgb(100, 100, 100)",
+  sidebarActiveClr: "rgb(180, 180, 180)",
   sidebarSelectedBg: "rgb(214, 87, 81)",
   sidebarSelectedClr: "rgb(255, 255, 255)",
 };
@@ -29,7 +30,7 @@ const darkTheme = {
   // sidebar section
   sidebarBg: "rgb(51, 51, 51)",
   sidebarHeadingClr: "rgb(255, 255, 255)",
-  sidebarLinkClr: "rgb(129, 129, 129)",
+  sidebarLinkClr: "rgb(180, 180, 180)",
   sidebarActiveClr: "rgb(241, 241, 241)",
   sidebarSelectedBg: "rgb(214, 87, 81)",
   sidebarSelectedClr: "rgb(255, 255, 255)",
